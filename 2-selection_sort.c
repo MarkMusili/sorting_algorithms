a2478b3fd5686e926350f14055028ed7ce1d2988
MarkMusili <musiliyrn@gmail.com>
2023-09-24 20:18:55 +0300

    Selection sort algorithm

---
+++ 2-selection_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+/**
+ * selectionSort - The selection sort algorithm
+ * @array: array to be sorted
+ * @size: size of the array
+ * Return: Nothing
+*/
+void selection_sort(int *array, size_t size)
+{
+    int idx, temp;
+    size_t i, j;
+
+    for (i = 0; i < size - 1; i++) {
+        idx = i;
+        
+        for (j = i + 1; j < size; j++) {
+            if (array[j] < array[idx]) {
+                idx = j;
+            }
+        }
+        
+        temp = array[i];
+        array[i] = array[idx];
+        array[idx] = temp;
+        print_array(array, size);
+    }
+}
