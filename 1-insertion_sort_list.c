ada4c89fe365f08ea2a2063afa1cd4f7416a3e96
Ote Ian <oteian9@gmail.com>
2023-09-24 19:29:54 +0300

    .

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers
+ *
+ * @list: Double pointer to the head node of a doubly linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *current = (*list)->next;
+    listint_t *temp;
+
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
+
+    while (current != NULL)
+    {
+        temp = current;
+        while (temp->prev != NULL && temp->n < temp->prev->n)
+        {
+            temp->prev->next = temp->next;
+
+            if (temp->next != NULL)
+                temp->next->prev = temp->prev;
+                
+            temp->next = temp->prev;
+            temp->prev = temp->prev->prev;
+            temp->next->prev = temp;
+
+            if (temp->prev == NULL)
+                *list = temp;
+            else
+                temp->prev->next = temp;
+
+            print_list(*list);
+        }
+        current = current->next;
+    }
+}
