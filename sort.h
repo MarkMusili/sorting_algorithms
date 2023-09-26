58e9fc1695e7d08a67e457b2e7e40be1d23bc2b6
Ote Ian <oteian9@gmail.com>
2023-09-26 23:17:01 +0300

    Header file

--- sort.h
+++ sort.h
@@ -1,6 +1,7 @@
 #ifndef SORT_H
 #define SORT_H
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -23,7 +24,8 @@ void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
-int partition(int *array, int low, int high);
+void quick_sort_recursive(int *array, ssize_t start, ssize_t end, ssize_t size);
+int partition(int *array, ssize_t start, ssize_t end, ssize_t size);
 void swap(int *a, int *b);
 
 #endif
