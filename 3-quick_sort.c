2456bd3a554e2bf3268ca7a64a80c53e776bddcb
Ote Ian <oteian9@gmail.com>
2023-09-27 00:11:50 +0300

    Quick sort

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,17 +1,5 @@
 #include "sort.h"
-
-/**
- * swap - function that swaps the position of two integers
- *
- * @a: Pointer to the firs integer
- * @b: Pointer to the second integer
- */
-void swap(int *a, int *b)
-{
-	int temp = *a;
-	*a = *b;
-	*b = temp;
-}
+#include "swap.c"
 
 /**
  * quick_sort - Implementation of quick sort algorithm
